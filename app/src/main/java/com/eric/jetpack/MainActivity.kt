831df800e76f57e53f547f092314b7c55bc5bb5a
satriawarn <satriawarn@gmail.com>
2023-02-23 10:36:59 +0700

    align composables with wrapped content

--- app/src/main/java/com/eric/jetpack/MainActivity.kt
+++ app/src/main/java/com/eric/jetpack/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,6 +29,17 @@ fun MainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
+        Surface(
+            color = Color.Magenta,
+            modifier = Modifier.wrapContentSize(
+                align = Alignment.BottomStart
+            )
+        ) {
+            Text(
+                text = "Wrapped Content",
+                style = MaterialTheme.typography.h4
+            )
+        }
 
     }
 }
